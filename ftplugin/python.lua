f92dc027013a139cc5b13ffd51d4676b3a97129f
Azsu-rae <aitameurmedilyas@gmail.com>
2025-12-20 21:26:22 +0100

    '.\init.luas'

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,4 +1,6 @@
 
+vim.treesitter.start()
+
 vim.keymap.set("n", "<leader>c", function()
   vim.cmd(string.format("split | term ipython -i '%s'", vim.fn.expand('%:p')))
 end)
--- init.lua
+++ init.lua
@@ -1,14 +1,17 @@
 
+local windows = vim.uv.os_uname().sysname == "Windows_NT"
+local vimrc = "~/.vimrc" and not windows or "~/_vimrc"
+
 vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter",
   "https://github.com/mason-org/mason.nvim",
   "https://github.com/EdenEast/nightfox.nvim",
 })
 
-vim.cmd.colorscheme('carbonfox')
-vim.cmd.source("~/.vimrc")
+vim.cmd.colorscheme("carbonfox")
+vim.cmd.source(vimrc)
 
-require('nvim-treesitter').install({"lua", "python"})
+require("nvim-treesitter").install({"lua", "python"})
 require("mason").setup()
 
 vim.lsp.enable({"pylsp", "lua_ls"})
@@ -16,12 +19,12 @@ vim.diagnostic.config {
   virtual_text = true
 }
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  pattern = '*',
-  desc = 'Highlight when yanking (copying) text',
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  desc = "Highlight when yanking (copying) text",
   callback = function ()
     vim.highlight.on_yank {
-      higroup = 'IncSearch',
+      higroup = "IncSearch",
       timeout = 200,
     }
   end
@@ -29,7 +32,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.keymap.set("n", "<leader>st", function()
   local win_id = vim.api.nvim_get_current_win()
-  vim.cmd('tab split')
+  vim.cmd("tab split")
   vim.api.nvim_win_close(win_id, true)
 end, { desc="[s]plit into a new [t]ab" })
 
@@ -41,5 +44,5 @@ vim.keymap.set("n", "<leader>git", function()
     return
   end
 
-  vim.cmd('split | term git add . && git commit -m "%s" && git push origin main')
+  vim.cmd("split | term git add . && git commit -m '%s' && git push origin main")
 end)
