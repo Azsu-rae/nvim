ebd5f3a78208117ef5e0c46c329b14621de68ec5
Azsu-rae <aitameurmedilyas@gmail.com>
2026-02-26 20:59:11 +0100

    sync

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,5 +1,1 @@
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#ffa066", bg = "NONE" })
-
--- vim.api.nvim_set_hl(0, "VertSplit", { fg = "#ff0000" })   -- legacy name
--- vim.api.nvim_set_hl(0, "StatusLine", { bg = "#222222" })
--- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#111111" })
