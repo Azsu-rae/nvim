08ef0a97d3b2d7ab4f805e20189b641704c4e4a5
rayane <khelfaatr@gmail.com>
2026-03-04 12:06:24 +0100

    sync

--- init.lua
+++ init.lua
@@ -1,10 +1,6 @@
-if vim.g.neovide then
-  vim.o.guifont = "jetBrainsMono Nerd Font"
-end
 
-local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
-local vimrc = is_windows and "~/_vimrc" or "~/.vimrc"
-vim.cmd.source(vimrc)
+require("osconfig")
+require("editor")
 
 require("config.lazy")
 
---
+++ lua/editor.lua
@@ -0,0 +1,5 @@
+
+if vim.g.neovide then
+  vim.o.guifont = "jetBrainsMono Nerd Font"
+end
+
