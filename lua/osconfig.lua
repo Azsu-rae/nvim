037e55a2b5e5b846b0245bd95ca3fe0ccfab12a0
Azsu-rae <aitameurmedilyas@gmail.com>
2026-03-15 10:49:41 +0100

    powerhsell support

--- lua/osconfig.lua
+++ lua/osconfig.lua
@@ -1,8 +1,15 @@
 
-local in_win = (vim.fn.has("win32") == 1)
-if in_win then
+local M = {}
+
+M.in_win = (vim.fn.has("win32") == 1)
+M.ext = M.in_win and "ps1"  or "sh"
+vim.notify("extension: " .. M.ext)
+
+if M.in_win then
   vim.o.shell = "pwsh"
   vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
   vim.o.shellquote = ""
   vim.o.shellxquote = ""
 end
+
+return M
