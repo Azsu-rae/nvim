efa9e283d5d1f7477ca44e72265deedbccd6220d
Azsu-rae <aitameurmedilyas@gmail.com>
2026-04-01 10:54:22 +0100

    adding typst

--- ftplugin/typst.lua
+++ ftplugin/typst.lua
@@ -3,7 +3,9 @@ local file_name = vim.fn.fnamemodify(file_path, ':t:r')
 
 local output_path = vim.fn.fnamemodify(file_path, ":h") .. "/build"
 local pdf_path = string.format("%s/%s.pdf", output_path, file_name)
+
 local watch_job = nil
+local pdf_job = nil
 
 vim.keymap.set("n", "<leader>wtch", function()
   if watch_job then
@@ -25,6 +27,20 @@ vim.keymap.set("n", "<leader>wtch", function()
   vim.notify("Started typst watcher!")
 end)
 
-vim.keymap.set("n", "<leader>xdg", function()
-  vim.cmd(string.format("!xdg-open '%s'", pdf_path))
+vim.keymap.set("n", "<leader>pdf", function()
+  if pdf_job then
+    vim.notify("PDF is already opened")
+    return
+  end
+
+  pdf_job = vim.fn.jobstart({
+    "sioyek",
+    pdf_path,
+  }, {
+    on_exit = function()
+      pdf_job = nil
+    end,
+  })
+
+  vim.notify("Started sioyek!")
 end)
