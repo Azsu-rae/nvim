7c67fd7d5a3e8bf6289df3eea82888c33083a6cc
Azsu-rae <aitameurmedilyas@gmail.com>
2026-04-10 01:55:12 +0100

    neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,22 +1,24 @@
 return {
-    --        
-    --        
-    --          🌀   ✱   ⭒   ✦   *
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons",
-    },
-    lazy = false,
-    config = function()
-        require("neo-tree").setup({
-            filesystem = {
-                filtered_items = {
-                    hide_dotfiles = false,
-                },
-            },
-        })
-    end,
+  --        
+  --        
+  --          🌀   ✱   ⭒   ✦   *
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false,
+  config = function()
+    vim.keymap.set("n", "<leader>e", "<CMD>Neotree filesystem reveal toggle<CR>")
+    vim.keymap.set("n", "<leader>ls", "<CMD>Neotree action=focus source=buffers position=float toggle=true<CR>")
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    })
+  end,
 }
