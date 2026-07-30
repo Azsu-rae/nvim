8d71d91bffe8a8ff12f0a512b1055a19ae0684d2
Azsu-rae <aitameurmedilyas@gmail.com>
2026-07-30 18:48:01 +0100

    try ... catch surround

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,8 +4,10 @@ return {
     require("toggleterm").setup({
       open_mapping = "<c-\\>"
     })
+
     vim.keymap.set({ "n", "t" }, "<c-|>", "<CMD>ToggleTerm direction=float<CR>")
     vim.keymap.set("t", "<c-|>", "<CMD>ToggleTerm direction=float<CR>")
+
     vim.keymap.set("n", "<leader>sc", "<CMD>ToggleTermSendCurrentLine<CR>")
     vim.keymap.set("v", "<leader>sv", "<CMD>ToggleTermSendVisualLines<CR>")
     vim.keymap.set("v", "<leader>ss", "<CMD>ToggleTermSendVisualSelection<CR>")
